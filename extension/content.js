8598ac4d689847993f15225aa382f0814b0b4b75
sheridanvk <sheridan.kates@gmail.com>
2018-08-30 22:29:35 -0400

    Saving progress on highlighting elements on the page

--- extension/content.js
+++ extension/content.js
@@ -1,25 +1,36 @@
 /*global chrome*/
 
 // content.js
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if( request.message === "clicked_browser_action" ) {
-      console.log("hello extension!");
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.message === "clicked_browser_action") {
+    console.log("hello extension!");
 
-      var div = $("<div>");
+    var highlightDiv = $('<div>')
+    highlightDiv.attr('id', 'page-scraper-highlighted')
 
-      div.css({
-        position: 'fixed',
-        left: '0px',
-        bottom: '0px',
-        width: '100%',
-        height: '20vh',
-        border: '1px solid black'
-      });
-      div.text('hello from Lincoln and Sheridan')
+    $('body').mouseover(function(e) {
+      console.log(e);
+      var highlightTarget = $(e.target)
+      highlightDiv.offset(highlightTarget.offset())
+      highlightDiv.width(highlightTarget.width())
+      highlightDiv.height(highlightTarget.height())
+    })
+    
+    var div = $("<div>");
 
-      div.appendTo(document.body);
-    }
+    div.attr('id', 'page-scraper')
+    div.css({
+      position: 'fixed',
+      left: '0px',
+      bottom: '0px',
+      width: '100%',
+      height: '20vh',
+      border: '1px solid black',
+      backgroundColor: 'white'
+    });
+    div.text('hello from Lincoln and Sheridan')
+
+    div.appendTo(document.body);
+    highlightDiv.appendTo(document.body);
   }
-);
-
+});
